84630bccc9f5ed0df3f88958372edb0079f2a8b1
Shahar Yifrah <shaharyi88@gmail.com>
2022-10-11 13:20:26 +0300

    added package doc file

--- src/main/java/com/shaharyi/sheep/Board.java
+++ src/main/java/com/shaharyi/sheep/Board.java
@@ -5,49 +5,49 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class Board {
-	private char[][] m;
-	private boolean[][] v;
+	private char[][] matrix;
+	private boolean[][] visited;
 	private int rows;
 	private int cols;
 
 	public Board(int r, int c) {
-		m = new char[r][c];
+		matrix = new char[r][c];
 		rows = r;
 		cols = c;
 	}
 
 	public boolean isFenced(int[] t) {
-		v = new boolean[rows][cols];
+		visited = new boolean[rows][cols];
 		return isFenced(t[0], t[1]);
 	}
 
 	public boolean isFenced(int r, int c) {
-		System.out.println(r+","+c);
+		System.out.println(r + ", " + c);
 		if (r < 0 || r > rows || c < 0 || c > cols)
 			return false;
-		if (v[r][c])
+		if (visited[r][c])
 			return true;
-		v[r][c] = true;    // mark visited
-		if (m[r][c] == '#')
+		visited[r][c] = true;    // mark visited
+		if (matrix[r][c] == '#')
 			return true;
 		return isFenced(r - 1, c) && isFenced(r, c + 1) && isFenced(r + 1, c) && isFenced(r, c - 1);
 	}
 
 	public int fenced(int[] t) {
-		v = new boolean[rows][cols];
+		visited = new boolean[rows][cols];
 		return fenced(t[0], t[1]);
 	}
 
 	public int fenced(int r, int c) {
 		if (r < 0 || r > rows || c < 0 || c > cols)
 			return -1;
-		if (v[r][c])
+		if (visited[r][c])
 			return 0;
-		v[r][c] = true;    // mark visited
-		if (m[r][c] == '#')
+		visited[r][c] = true;    // mark visited
+		if (matrix[r][c] == '#')
 			return 0;
 		int s = 0;
-		if (m[r][c] == 'S')
+		if (matrix[r][c] == 'S')
 			s = 1;
 		return s + fenced(r - 1, c) + fenced(r, c + 1) + fenced(r + 1, c) + fenced(r, c - 1);
 	}
@@ -57,12 +57,12 @@ public class Board {
 		Scanner scanner = new Scanner(file);
 		rows = scanner.nextInt();
 		cols = scanner.nextInt();
-		m = new char[rows][cols];
+		matrix = new char[rows][cols];
 		int r = 0;
 		while (scanner.hasNextLine()) {
 			String line = scanner.nextLine();
 			for (int c = 0; c < line.length(); c++) {
-				m[r][c] = line.charAt(c);
+				matrix[r][c] = line.charAt(c);
 			}
 			r++;
 		}
@@ -70,18 +70,18 @@ public class Board {
 	}
 
 	public int[] find(char x) {
-		for (int i = 0; i < m.length; i++) {
-			for (int j = 0; j < m[0].length; j++) {
-				if (m[i][j] == x)
+		for (int i = 0; i < matrix.length; i++) {
+			for (int j = 0; j < matrix[0].length; j++) {
+				if (matrix[i][j] == x)
 					return new int[] { i, j };
 			}
 		}
 		return null;
 	}
 	public void print() {
-		for (int i = 0; i < m.length; i++) {
-			for (int j = 0; j < m[0].length; j++) {
-				System.out.print(m[i][j]);
+		for (int i = 0; i < matrix.length; i++) {
+			for (int j = 0; j < matrix[0].length; j++) {
+				System.out.print(matrix[i][j]);
 			}
 			System.out.println();
 		}
---
+++ src/main/java/com/shaharyi/strategy/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * com.shaharyi.strategy lets you play against the computer several versions of Tic-Tac-Toe  
+ */
+package com.shaharyi.strategy;
