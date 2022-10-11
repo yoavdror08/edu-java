5c679942fdcc0b7a9fe4f0189bcf10563395440b
Shahar Yifrah <shaharyi88@gmail.com>
2022-10-11 22:53:25 +0300

    minor

--- src/main/java/com/shaharyi/soduku/MainQ.java
+++ src/main/java/com/shaharyi/soduku/MainQ.java
@@ -7,24 +7,26 @@ public class MainQ {
 
 	static boolean solveSudoku(int grid[][], int row, int col)
 	{
-					return true;
+		return true;
 	}
 
 	/* A utility function to print grid */
-
+	public static void print(int grid[][]) {
+		
+	}
 	
 	// Driver Code
 	public static void main(String[] args)
 	{
-		int grid[][] = { { 3, 0, 6, 5, 0, 8, 4, 0, 0 },
-						{ 5, 2, 0, 0, 0, 0, 0, 0, 0 },
-						{ 0, 8, 7, 0, 0, 0, 0, 3, 1 },
-						{ 0, 0, 3, 0, 1, 0, 0, 8, 0 },
-						{ 9, 0, 0, 8, 6, 3, 0, 0, 5 },
-						{ 0, 5, 0, 0, 9, 0, 6, 0, 0 },
-						{ 1, 3, 0, 0, 0, 0, 2, 5, 0 },
-						{ 0, 0, 0, 0, 0, 0, 0, 7, 4 },
-						{ 0, 0, 5, 2, 0, 6, 3, 0, 0 } };
+		int grid[][] = { 	{ 3, 0, 6, 5, 0, 8, 4, 0, 0 },
+							{ 5, 2, 0, 0, 0, 0, 0, 0, 0 },
+							{ 0, 8, 7, 0, 0, 0, 0, 3, 1 },
+							{ 0, 0, 3, 0, 1, 0, 0, 8, 0 },
+							{ 9, 0, 0, 8, 6, 3, 0, 0, 5 },
+							{ 0, 5, 0, 0, 9, 0, 6, 0, 0 },
+							{ 1, 3, 0, 0, 0, 0, 2, 5, 0 },
+							{ 0, 0, 0, 0, 0, 0, 0, 7, 4 },
+							{ 0, 0, 5, 2, 0, 6, 3, 0, 0 } };
 
 		if (solveSudoku(grid, 0, 0))
 			print(grid);
