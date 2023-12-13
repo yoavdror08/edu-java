e17f1d113bf24f0e9d3416ed5c7d4b67a23d4381
Shachar Ifrach <33417983+shaharyi@users.noreply.github.com>
2023-12-13 12:46:38 +0200

    Update Main.java

--- src/main/java/com/shaharyi/node/Main.java
+++ src/main/java/com/shaharyi/node/Main.java
@@ -1,8 +1,13 @@
 package com.shaharyi.node;
 
-public class Main {
-
+public class Main {	
+	
 	public static void main(String[] args) {
+		testIntNode();
+		testStack();
+	}
+
+	public static void testStack() {
 		Stack<Integer> c = new Stack<>();
 		c.push(3);
 		c.push(5);
@@ -16,8 +21,8 @@ public class Main {
 		d.push(10);
 		Stack<Integer> r = merge2(c, d);
 		System.out.println(r);
-	}
-
+	}		
+	
 	public static Stack<Integer> merge(Stack<Integer> a, Stack<Integer> b) {
 		Stack<Integer> c = new Stack<>();
 
@@ -50,5 +55,21 @@ public class Main {
 				c.push(b.pop());
 		}
 		return c;
+	}
+
+	public static IntNode build(int[] a) {
+		IntNode p = new IntNode(a[0]);
+		IntNode h = p;
+		for (int i = 1; i < a.length; i++) {
+			p.setNext(new IntNode(a[i]));
+			p = p.getNext();
+		}
+		return h;
+	}
+	
+	public static void testIntNode() {
+		int[] b = { 1,1,5,1,2,4,8 };
+		IntNode p = build(b);
+		System.out.println(p);
 	}
 }
