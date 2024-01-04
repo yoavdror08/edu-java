874eb1777784f00219e91f1076f05bb72f3423e8
Shachar Ifrach <33417983+shaharyi@users.noreply.github.com>
2024-01-04 09:15:48 +0200

    Create Main.java

---
+++ src/main/book_ans/cards/Main.java
@@ -0,0 +1,34 @@
+class Main {
+    public static void main(String[] args) {
+        Player p1 = new Player("a");
+        Player p2 = new Player("b");
+        Player p3 = new Player("c");
+        Player p4 = new Player("d");
+        int round = 0;
+        boolean won = false;
+        while (won == false) {
+            if (round % 4 == 0) {
+                p1.throwCard();
+                p1.addCard();
+                System.out.println(p1);
+                won = p1.playerWon();
+            } else if (round % 4 == 1) {
+                p2.throwCard();
+                p2.addCard();
+                System.out.println(p2);
+                won = p2.playerWon();
+            } else if (round % 4 == 2) {
+                p3.throwCard();
+                p3.addCard();
+                System.out.println(p3);
+                won = p3.playerWon();
+            } else if (round % 4 == 3) {
+                p4.throwCard();
+                p4.addCard();
+                System.out.println(p4);
+                won = p4.playerWon();
+            }
+            round++;
+        }
+    }
+}
