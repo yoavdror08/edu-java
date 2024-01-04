7d5b6b2c472ba1c4c503757c2465569405239aa8
Shachar Ifrach <33417983+shaharyi@users.noreply.github.com>
2024-01-04 09:56:31 +0200

    Update Main_arr.java

--- src/main/book_ans/cards/Main_arr.java
+++ src/main/book_ans/cards/Main_arr.java
@@ -3,11 +3,11 @@ class Main {
         int numPlayers = 4;
         Player p[] = new Player[numPlayers];
         int i;
-        for (i=0; i<numPlayers; i++)
+        for (i = 0; i < numPlayers; i++)
             p[i] = new Player("Player" + i);
         int round = 0;
         boolean won = false;
-        while (won == false) {
+        while (!won && round < 30) {
             i = round % 4;
             p[i].throwCard();
             p[i].addCard();
