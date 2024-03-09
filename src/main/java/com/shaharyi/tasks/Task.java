72515418d408f963c09e0b6e2be2e3f6567ac143
Shachar Ifrach <33417983+shaharyi@users.noreply.github.com>
2024-03-09 20:16:42 +0200

    Create Task.java

---
+++ src/main/java/com/shaharyi/tasks/Task.java
@@ -0,0 +1,42 @@
+
+public class Task {
+	private String id;
+	private int priority; // 1 is highest
+	private int credit;
+
+	public String getId() {
+		return id;
+	}
+
+	public int getPriority() {
+		return priority;
+	}
+
+	public int getCredit() {
+		return credit;
+	}
+
+	public Task(String id, int priority, int credit) {
+		super();
+		this.id = id;
+		this.priority = priority;
+		this.credit = credit;
+	}
+
+	public void decPriority() {
+		priority++;
+	}
+
+	public void addCredit(int c) {
+		credit += c;
+	}
+
+	public void subCredit(int c) {
+		credit -= c;
+	}
+
+	public String toString() {
+		return "Task [id=" + id + ", priority=" + priority + ", credit=" + credit + "]";
+	}
+	
+}
