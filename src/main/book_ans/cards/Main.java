725232962b692894c1b160b88eaca68da154f9cb
Shachar Ifrach <33417983+shaharyi@users.noreply.github.com>
2024-04-25 13:39:34 +0300

    Update Main.java

--- src/main/book_ans/cards/Main.java
+++ src/main/book_ans/cards/Main.java
@@ -31,4 +31,21 @@ class Main {
             round++;
         }
     }
+    public static void main2(String[] args) {
+        int numPlayers = 4;
+        Player p[] = new Player[numPlayers];
+        int i;
+        for (i = 0; i < numPlayers; i++)
+            p[i] = new Player("Player" + i);        
+        int round = 0;
+        boolean won = false;
+        while (won == false) {
+            i = round % 4;
+            p[i].throwCard();
+            p[i].addCard();
+            System.out.println(p[i]);
+            won = p[i].playerWon();
+            round++;
+        }
+    }    
 }
