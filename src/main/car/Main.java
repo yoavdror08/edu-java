6e0548723491c49af240cec3f9b231a2153d5e66
Shachar Ifrach <33417983+shaharyi@users.noreply.github.com>
2024-10-06 12:34:28 +0300

    Add files via upload

--- src/main/car/Main.java
+++ src/main/car/Main.java
@@ -9,9 +9,14 @@ public class Main {
 		cars2[0] = new CarInfo("c20", false, 45);
 		cars2[1] = new CarInfo("c21", false, 50);
 
+		CarInfo[] cars3 = new CarInfo[2];
+		cars3[0] = new CarInfo("c30", false, 90);
+		cars3[1] = new CarInfo("c31", false, 120);
+
 		CameraInfo[] cameras = new CameraInfo[200];
 		cameras[0] = new CameraInfo(12, 110, cars1);
 		cameras[1] = new CameraInfo(12, 50, cars2);
+		cameras[3] = new CameraInfo(11, 120, cars3);
 
 		int good = legalCities(cameras);
 	}
