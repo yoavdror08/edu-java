3f4f003fc2fef6514ec16e6e88f82464d3d386bd
Shachar Ifrach <33417983+shaharyi@users.noreply.github.com>
2024-11-21 23:22:37 +0200

    Create Main.java

---
+++ src/main/box/Main.java
@@ -0,0 +1,10 @@
+class Main {
+  public static void main(String[] args) {
+    Box<Integer> b1 = new Box<>(4);
+    Box<String> b2 = new Box<>("cat");
+    System.out.println(b1);
+    System.out.println(b2);
+ 
+
+  }
+}
